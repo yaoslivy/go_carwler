ed81fcb7649226739229a0f86694b74e41405536
yaoshuilin <42492091+yaoslivy@users.noreply.github.com>
2022-07-06 14:58:11 +0800

    feat(scheduler): 新增scheduler模块
    将之间任务队列的方式改为了管道的方式

--- main.go
+++ main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
-	"go_carwler/zhenai/engine"
-	"go_carwler/zhenai/parser"
-	"go_carwler/zhenai/types"
+	"go_carwler/engine"
+	"go_carwler/parser"
+	"go_carwler/scheduler"
+	"go_carwler/types"
 )
 
 func main() {
-	engine.Run(types.Request{
+	//engine.SingleEngine{}.Run(engineTypes.Request{
+	//	Url:       "https://www.7799520.com/jiaou", //从征婚网城市列表页面出发
+	//	ParseFunc: parser.ParseCityList,
+	//})
+	//http://www.7799520.com/user/6056626.html
+
+	concurrentEngine := engine.ConcurrentEngine{
+		Scheduler:   &scheduler.SimpleScheduler{},
+		WorkerCount: 10,
+	}
+	concurrentEngine.Run(types.Request{
 		Url:       "https://www.7799520.com/jiaou", //从征婚网城市列表页面出发
 		ParseFunc: parser.ParseCityList,
 	})
-	//http://www.7799520.com/user/6056626.html
-
 }
